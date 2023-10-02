00949db4bc40ee4be18810dd66786eccb20a908e
tiago-clementino <tiago.luks@gmail.com>
2023-10-02 08:50:23 -0300

    Validation model analysis

--- traders_life.R
+++ traders_life.R
@@ -6,33 +6,9 @@ library(lubridate)
 library(here)
 library(scales)
 library(latex2exp)
+library(ggcorrplot)
 
-#gráfico de barras para o resultado com CI real
-
-#gráfico de linha da quantidade de transações recusadas por ciclo em fução da confiança (use a confiança por feedback, por web of trust, com e sem classificacao de transações)
-
-#df <-
-#  list.files(path = "../economy_simulation/output/", pattern = "timeline*.csv", full.names = TRUE) %>% 
-#  lapply(function(x) read_csv(x , 
-#                              col_names = TRUE, col_types = cols(
-#                                run = col_character(),
-#                                hash = col_integer(),
-#                                type = col_integer(),
-#                                notMatch = col_integer(),
-#                                memory = col_integer(),
-#                                hasValidator = col_integer(),
-#                                typeAgnostic = col_integer(),
-#                                securityDeposit = col_integer(),
-#                                hasFeedback = col_integer(),
-##                                feedbackPercent = col_number(),
-#                                honestPercent = col_number(),
-#                                stepCount = col_integer(),
-#                                totalTransactions = col_integer(),
-#                                transactionFail = col_integer(),
-##                                AvoidedFailTransactions = col_integer()
-#                              ))) %>%                              # Store all files in list
-#  bind_rows
-#df  
+library(reshape2)
 
 
 df <-
@@ -45,6 +21,112 @@ colnames(df) <- c("run","hash","type","notMatch","memory","hasValidator","typeAg
                   "totalTransactions","transactionFail","AvoidedFailTransactions")
 
 as.data.frame(df)
+#count(df)
+df<- df %>% filter(totalTransactions >= 100)
+df<-df[sample(nrow(df), 400), ]
+df %>% 
+  mutate(full_fail_transactions = transactionFail - notMatch,
+         sucessTransactions = totalTransactions - (transactionFail - notMatch),
+         honestidade = sapply(honestPercent*100, as.factor),
+         count=n(),
+         profile = case_when(
+           hasValidator == 1 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 0 ~ "A",
+           hasValidator == 1 & typeAgnostic == 1 & securityDeposit == 0 & hasFeedback == 0 ~ "B",
+           hasValidator == 0 & typeAgnostic == 0 & securityDeposit == 1 & hasFeedback == 0 ~ "C",
+           hasValidator == 0 & typeAgnostic == 1 & securityDeposit == 1 & hasFeedback == 0 ~ "D",
+           hasValidator == 1 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 1 ~ "E",
+           hasValidator == 1 & typeAgnostic == 1 & securityDeposit == 0 & hasFeedback == 1 ~ "F",
+           hasValidator == 0 & typeAgnostic == 0 & securityDeposit == 1 & hasFeedback == 1 ~ "G",
+           hasValidator == 0 & typeAgnostic == 1 & securityDeposit == 1 & hasFeedback == 1 ~ "H",
+           hasValidator == 0 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 0 ~ "I",
+           ),
+         full_fail_transactions = case_when(
+           hasValidator == 1 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 0 ~ full_fail_transactions,
+           hasValidator == 1 & typeAgnostic == 1 & securityDeposit == 0 & hasFeedback == 0 ~ full_fail_transactions,
+           hasValidator == 0 & typeAgnostic == 0 & securityDeposit == 1 & hasFeedback == 0 ~ full_fail_transactions,
+           hasValidator == 0 & typeAgnostic == 1 & securityDeposit == 1 & hasFeedback == 0 ~ full_fail_transactions,
+           hasValidator == 1 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 1 ~ full_fail_transactions,
+           hasValidator == 1 & typeAgnostic == 1 & securityDeposit == 0 & hasFeedback == 1 ~ full_fail_transactions,
+           hasValidator == 0 & typeAgnostic == 0 & securityDeposit == 1 & hasFeedback == 1 ~ full_fail_transactions,
+           hasValidator == 0 & typeAgnostic == 1 & securityDeposit == 1 & hasFeedback == 1 ~ full_fail_transactions,
+           hasValidator == 0 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 0 ~ full_fail_transactions,
+         )
+         ) %>%
+  ggplot() +
+  #geom_count(aes(x = hash, y = full_fail_transactions, group = honestPercent, col = honestidade)) + 
+  geom_point(aes(x = hash, y = sucessTransactions, col = paste(honestidade, "%", sep = "")))+#, alpha = honestPercent)) + 
+  #scale_x_continuous(breaks = 1:12) + 
+  #scale_fill_gradient(low = "red", high = "green", limits = c(-8, 8), labels = percent) +
+  facet_wrap(~profile) +
+  labs(y='Successful Transactions',  
+       x="Time (steps)", 
+       #title="Comparativo de Correlação entre Honestidade e Sucesso ", 
+       #subtitle="(Sucesso da População, Taxa de Honestidade na População)",
+       color="Honesty")+
+  theme(plot.title = element_text(face="bold",size = "15"),
+        plot.subtitle = element_text(size = "10"),
+        plot.caption = element_text(size="10"),
+        axis.title.y = element_text(size="12"),
+        axis.text.x = element_text(size="10"),
+        axis.text.y = element_text(size="12"),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank(),
+        #panel.background = element_blank(),
+        legend.position = "bottom",
+        panel.border=element_blank())
+
+
+df %>% 
+  mutate(full_fail_transactions = transactionFail - notMatch,
+         sucessTransactions = totalTransactions - (transactionFail - notMatch),
+         honestidade = sapply(honestPercent*100, as.factor),
+         count=n(),
+         profile = case_when(
+           hasValidator == 1 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 0 ~ "A",
+           hasValidator == 1 & typeAgnostic == 1 & securityDeposit == 0 & hasFeedback == 0 ~ "B",
+           hasValidator == 0 & typeAgnostic == 0 & securityDeposit == 1 & hasFeedback == 0 ~ "C",
+           hasValidator == 0 & typeAgnostic == 1 & securityDeposit == 1 & hasFeedback == 0 ~ "D",
+           hasValidator == 1 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 1 ~ "E",
+           hasValidator == 1 & typeAgnostic == 1 & securityDeposit == 0 & hasFeedback == 1 ~ "F",
+           hasValidator == 0 & typeAgnostic == 0 & securityDeposit == 1 & hasFeedback == 1 ~ "G",
+           hasValidator == 0 & typeAgnostic == 1 & securityDeposit == 1 & hasFeedback == 1 ~ "H",
+           hasValidator == 0 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 0 ~ "I",
+         ),
+         full_fail_transactions = case_when(
+           hasValidator == 1 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 0 ~ full_fail_transactions,
+           hasValidator == 1 & typeAgnostic == 1 & securityDeposit == 0 & hasFeedback == 0 ~ full_fail_transactions,
+           hasValidator == 0 & typeAgnostic == 0 & securityDeposit == 1 & hasFeedback == 0 ~ full_fail_transactions,
+           hasValidator == 0 & typeAgnostic == 1 & securityDeposit == 1 & hasFeedback == 0 ~ full_fail_transactions,
+           hasValidator == 1 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 1 ~ full_fail_transactions,
+           hasValidator == 1 & typeAgnostic == 1 & securityDeposit == 0 & hasFeedback == 1 ~ full_fail_transactions,
+           hasValidator == 0 & typeAgnostic == 0 & securityDeposit == 1 & hasFeedback == 1 ~ full_fail_transactions,
+           hasValidator == 0 & typeAgnostic == 1 & securityDeposit == 1 & hasFeedback == 1 ~ full_fail_transactions,
+           hasValidator == 0 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 0 ~ full_fail_transactions,
+         )
+  ) %>%
+  ggplot() +
+  #geom_count(aes(x = hash, y = full_fail_transactions, group = honestPercent, col = honestidade)) + 
+  geom_point(aes(x = hash, y = full_fail_transactions, col = paste(honestidade, "%", sep = "")))+#, alpha = honestPercent)) + 
+  #scale_x_continuous(breaks = 1:12) + 
+  #scale_fill_gradient(low = "red", high = "green", limits = c(-8, 8), labels = percent) +
+  facet_wrap(~profile) +
+  labs(y='Fail Transactions',  
+       x="Time (steps)", 
+       #title="Comparativo de Correlação entre Honestidade e Sucesso ", 
+       #subtitle="(Sucesso da População, Taxa de Honestidade na População)",
+       color="Honesty")+
+  theme(plot.title = element_text(face="bold",size = "15"),
+        plot.subtitle = element_text(size = "10"),
+        plot.caption = element_text(size="10"),
+        axis.title.y = element_text(size="12"),
+        axis.text.x = element_text(size="10"),
+        axis.text.y = element_text(size="12"),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank(),
+        #panel.background = element_blank(),
+        legend.position = "bottom",
+        panel.border=element_blank())
+
 
 df %>% 
   mutate(full_fail_transactions = transactionFail - notMatch,
@@ -59,46 +141,134 @@ df %>%
            hasValidator == 1 & typeAgnostic == 1 & securityDeposit == 0 & hasFeedback == 1 ~ "F",
            hasValidator == 0 & typeAgnostic == 0 & securityDeposit == 1 & hasFeedback == 1 ~ "G",
            hasValidator == 0 & typeAgnostic == 1 & securityDeposit == 1 & hasFeedback == 1 ~ "H",
-           )
-         #day = formatC(day, width = 2, flag = "0"),
-         #month = lubridate::ymd(paste0(year, month, day)),
-         #month = lubridate::floor_date(month, "month")
-         ) %>% 
-  ggplot() +
-  #geom_count(aes(x = hash, y = full_fail_transactions, group = honestPercent, col = honestidade)) + 
-  geom_point(aes(x = hash, y = full_fail_transactions, col = paste(honestidade, "%", sep = ""), alpha = count)) + 
-  #scale_x_continuous(breaks = 1:12) + 
-  #scale_fill_gradient(low = "red", high = "green", limits = c(-8, 8), labels = percent) +
-  facet_wrap(~profile) +
-  labs(#x='Taxa de honestidade (%)',  
-       #y="Percentual de sucesso da população (%)", 
-       #title="Comparativo de Correlação entre Honestidade e Sucesso ", 
-       #subtitle="(Sucesso da População, Taxa de Honestidade na População)",
-       color="Honestidade")
-
-
-
-df %>% 
-  mutate(profile = case_when(
-           hasValidator == 1 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 0 ~ "A",
-           hasValidator == 1 & typeAgnostic == 1 & securityDeposit == 0 & hasFeedback == 0 ~ "B",
-           hasValidator == 0 & typeAgnostic == 0 & securityDeposit == 1 & hasFeedback == 0 ~ "C",
-           hasValidator == 0 & typeAgnostic == 1 & securityDeposit == 1 & hasFeedback == 0 ~ "D",
-           hasValidator == 1 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 1 ~ "E",
-           hasValidator == 1 & typeAgnostic == 1 & securityDeposit == 0 & hasFeedback == 1 ~ "F",
-           hasValidator == 0 & typeAgnostic == 0 & securityDeposit == 1 & hasFeedback == 1 ~ "G",
-           hasValidator == 0 & typeAgnostic == 1 & securityDeposit == 1 & hasFeedback == 1 ~ "H",
+           hasValidator == 0 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 0 ~ "I",
+         ),
+         AvoidedFailTransactions = case_when(
+           hasValidator == 1 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 0 ~ AvoidedFailTransactions,
+           hasValidator == 1 & typeAgnostic == 1 & securityDeposit == 0 & hasFeedback == 0 ~ AvoidedFailTransactions,#2
+           hasValidator == 0 & typeAgnostic == 0 & securityDeposit == 1 & hasFeedback == 0 ~ AvoidedFailTransactions,#10
+           hasValidator == 0 & typeAgnostic == 1 & securityDeposit == 1 & hasFeedback == 0 ~ AvoidedFailTransactions,#10
+           hasValidator == 1 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 1 ~ AvoidedFailTransactions,
+           hasValidator == 1 & typeAgnostic == 1 & securityDeposit == 0 & hasFeedback == 1 ~ AvoidedFailTransactions,
+           hasValidator == 0 & typeAgnostic == 0 & securityDeposit == 1 & hasFeedback == 1 ~ AvoidedFailTransactions,#10
+           hasValidator == 0 & typeAgnostic == 1 & securityDeposit == 1 & hasFeedback == 1 ~ AvoidedFailTransactions,#10
+           hasValidator == 0 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 0 ~ AvoidedFailTransactions,#10
          )
-         #day = formatC(day, width = 2, flag = "0"),
-         #month = lubridate::ymd(paste0(year, month, day)),
-         #month = lubridate::floor_date(month, "month")
-  ) %>% 
+  ) %>%
+  filter(hash <= 400) %>%
   ggplot() +
-  geom_line(aes(x = hash, y = AvoidedFailTransactions, group = honestPercent, col = honestPercent)) + 
+  #geom_line(aes(x = hash, y = AvoidedFailTransactions, group = honestPercent, col = honestPercent)) + 
+  geom_point(aes(x = hash, y = AvoidedFailTransactions, col = paste(honestidade, "%", sep = "")))+#, alpha = honestPercent)) + 
   #scale_x_continuous(breaks = 1:12) + 
-  facet_wrap(~profile)
-#gráfico de linha da quantidade de transações desonestas evidatas em todas as configurações de validador
+  facet_wrap(~profile) +
+  labs(y='Avoided Unsuccessful Transactions',  
+       x="Time (steps)", 
+    #title="Comparativo de Correlação entre Honestidade e Sucesso ", 
+    #subtitle="(Sucesso da População, Taxa de Honestidade na População)",
+    color="Honesty")+
+  theme(plot.title = element_text(face="bold",size = "15"),
+        plot.subtitle = element_text(size = "10"),
+        plot.caption = element_text(size="10"),
+        axis.title.y = element_text(size="12"),
+        axis.text.x = element_text(size="10"),
+        axis.text.y = element_text(size="12"),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank(),
+        #panel.background = element_blank(),
+        #legend.position = "bottom",
+        panel.border=element_blank())
+#quantidade de transações desonestas evidatas em todas as configurações de validador
 
 #mencione que desconsiderou o fato do depósito caução não ser eficaz em certos casos e mesmo assim ele se saiu pior
-#seria bom provar que o deposito caução não é eficaz em todos os casos
 
+
+# R program to illustrate
+# Spearman Correlation Testing
+
+# Converte para binario
+df_Binario <- df %>% mutate(full_fail_transactions = transactionFail - notMatch,
+                            sucessTransactions = totalTransactions - (transactionFail - notMatch),
+                            profile = case_when(
+                              hasValidator == 1 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 0 ~ "A",
+                              hasValidator == 1 & typeAgnostic == 1 & securityDeposit == 0 & hasFeedback == 0 ~ "B",
+                              hasValidator == 0 & typeAgnostic == 0 & securityDeposit == 1 & hasFeedback == 0 ~ "C",
+                              hasValidator == 0 & typeAgnostic == 1 & securityDeposit == 1 & hasFeedback == 0 ~ "D",
+                              hasValidator == 1 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 1 ~ "E",
+                              hasValidator == 1 & typeAgnostic == 1 & securityDeposit == 0 & hasFeedback == 1 ~ "F",
+                              hasValidator == 0 & typeAgnostic == 0 & securityDeposit == 1 & hasFeedback == 1 ~ "G",
+                              hasValidator == 0 & typeAgnostic == 1 & securityDeposit == 1 & hasFeedback == 1 ~ "H",
+                              hasValidator == 0 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 0 ~ "I",
+                            ),)%>%
+  filter(honestPercent == 0.4,sucessTransactions >= 0) %>%
+  select(-run,-hash,-type,-notMatch,-memory,-hasValidator,-typeAgnostic,
+         -securityDeposit,-hasFeedback,-feedbackPercent,-honestPercent,
+         -stepCount,-totalTransactions,-transactionFail,) #%>%
+  #binarize(
+  #  n_bins = 4, 
+   # thresh_infreq = 0.01
+ # )
+df_Binario
+
+# Exibe grafico
+df_Binario %>%
+  group_by(profile) %>% 
+  select_if(
+    is.double
+  ) %>% 
+  cor() %>% 
+  cbind(df_Binario$profile) %>%
+  ggcorrplot(hc.order = TRUE, 
+             outline.col = "white",
+             lab = TRUE ) + 
+  facet_wrap(~profile)
+
+df_Binario %>% 
+  ggcorrplot( ) + 
+  facet_wrap(~profile)
+
+
+
+get_lower_tri<-function(x){
+  x[upper.tri(x)] <- NA
+  return(x)
+}
+
+df_Binario_2 <- df_Binario
+
+colnames(df_Binario_2)[colnames(df_Binario_2) == "AvoidedFailTransactions"] ="Avoided"
+colnames(df_Binario_2)[colnames(df_Binario_2) == "sucessTransactions"] ="Successful"
+colnames(df_Binario_2)[colnames(df_Binario_2) == "full_fail_transactions"] ="Fail"
+
+df2 <- do.call(rbind, lapply(split(df_Binario_2[,1:3], df_Binario_2$profile), 
+                             function(x) melt(get_lower_tri(round(cor(x[1:3], method = 'spearman'), 3)),na.rm = FALSE)))
+
+type <- data.frame(profile=c("A","B","C","D","E","F","G","H","I"))
+type <- arrange(rbind(type,type,type,type,type,type,type,type,type),profile)
+
+my_cors <- cbind(type,df2)
+
+my_cors %>% 
+  filter(  !is.na(value)) %>%
+  ggplot(aes(Var1, Var2, fill = value)) + 
+  geom_tile() + 
+  geom_text(aes(label = round(value, 3)), color="black", size=3) +
+  scale_fill_gradient2(low = 'coral3', 
+                       high = 'green3', 
+                       mid = 'white', 
+                       midpoint = 0,
+                       limit = c(-1, 1), 
+                       space = "Lab", 
+                       name = 'Spearman\nCorrelation') + 
+  theme_grey()+
+  coord_fixed() +
+  theme(plot.title = element_text(face="bold",size = "15"),
+        plot.subtitle = element_text(size = "10"),
+        plot.caption = element_text(size="10"),
+        axis.title.y = element_blank(),
+        axis.title.x = element_blank(),
+        axis.text.x = element_text(size="10",angle = 90, vjust = 0.5, hjust=1),
+        axis.text.y = element_text(size="12"),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank(),
+        panel.border=element_blank())+
+  facet_wrap("profile",ncol = 5, nrow = 2)
--- traders_survivor_v2.R
+++ traders_survivor_v2.R
@@ -28,7 +28,7 @@ ecomony <-
 
 colnames(ecomony) <- c("sucesso","memoria","tem_validador","typeAgnostic","securityDeposit","hasFeedback",
                   "feedbackPercent","honestidade","stepCount","totalTransactions","transactionFail",
-                  "AvoidedFailTransactions","terminate")
+                  "AvoidedFailTransactions","uniqueValidators","terminate")
 
 as.data.frame(ecomony)
 
@@ -41,20 +41,61 @@ as.data.frame(ecomony)
 #
 # Remove jobs idênticos no mesmo build
 ecomony_2 = ecomony %>% 
+  mutate(profile = case_when(
+    tem_validador == 1 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 0 ~ "A",
+    tem_validador == 1 & typeAgnostic == 1 & securityDeposit == 0 & hasFeedback == 0 ~ "B",
+    tem_validador == 0 & typeAgnostic == 0 & securityDeposit == 1 & hasFeedback == 0 ~ "C",
+    tem_validador == 0 & typeAgnostic == 1 & securityDeposit == 1 & hasFeedback == 0 ~ "D",
+    tem_validador == 1 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 1 ~ "E",
+    tem_validador == 1 & typeAgnostic == 1 & securityDeposit == 0 & hasFeedback == 1 ~ "F",
+    tem_validador == 0 & typeAgnostic == 0 & securityDeposit == 1 & hasFeedback == 1 ~ "G",
+    tem_validador == 0 & typeAgnostic == 1 & securityDeposit == 1 & hasFeedback == 1 ~ "H",
+    tem_validador == 0 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 0 ~ "I",
+  ) ,
+  profile_count = case_when(
+    tem_validador == 1 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 0 ~ 1.4,
+    tem_validador == 1 & typeAgnostic == 1 & securityDeposit == 0 & hasFeedback == 0 ~ 1.2,
+    tem_validador == 0 & typeAgnostic == 0 & securityDeposit == 1 & hasFeedback == 0 ~ 1.0,
+    tem_validador == 0 & typeAgnostic == 1 & securityDeposit == 1 & hasFeedback == 0 ~ 0.8,
+    tem_validador == 1 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 1 ~ 0.6,
+    tem_validador == 1 & typeAgnostic == 1 & securityDeposit == 0 & hasFeedback == 1 ~ 0.4,
+    tem_validador == 0 & typeAgnostic == 0 & securityDeposit == 1 & hasFeedback == 1 ~ 0.2,
+    tem_validador == 0 & typeAgnostic == 1 & securityDeposit == 1 & hasFeedback == 1 ~ 0.0,
+    tem_validador == 0 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 0 ~ -0.2,
+  ) 
+  )%>% 
   filter(honestidade <= 0.9) %>%
-  group_by(honestidade, tem_validador) %>% 
-  summarise(acertou = (sum(sucesso)/n()),total = n()) 
+  group_by(honestidade, profile,profile_count) %>% 
+  summarise(acertou = (sum(sucesso)/n()),profile_count = first(profile_count), total = n()) 
 
-ecomony_2
-ggplot(data=ecomony_2, aes(y=acertou*100,x=honestidade*100,ymax = (acertou*97.5), ymin = if_else(acertou*102.5+2.5<100,acertou*102.5+2.5,100),  fill=if_else(tem_validador==0,"Não","Sim")))  +
-  geom_bar(stat="identity", position=position_dodge())+
-  geom_text(aes(label=paste0(round(acertou*100, 1), "")    ), vjust=-0.1, color="black", size=3.5)+
-  geom_errorbar() +
-  labs(x='Taxa de honestidade (%)',  
-       y="Percentual de sucesso da população (%)", 
+ecomony_3 = ecomony %>% 
+  mutate(profile = case_when(
+    tem_validador == 1 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 0 ~ "A",
+    tem_validador == 1 & typeAgnostic == 1 & securityDeposit == 0 & hasFeedback == 0 ~ "B",
+    tem_validador == 1 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 1 ~ "E",
+    tem_validador == 1 & typeAgnostic == 1 & securityDeposit == 0 & hasFeedback == 1 ~ "F",
+  ) ,
+  profile_count = case_when(
+    tem_validador == 1 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 0 ~ 1.65,
+    tem_validador == 1 & typeAgnostic == 1 & securityDeposit == 0 & hasFeedback == 0 ~ 0.95,
+    tem_validador == 1 & typeAgnostic == 0 & securityDeposit == 0 & hasFeedback == 1 ~ 0.25,
+    tem_validador == 1 & typeAgnostic == 1 & securityDeposit == 0 & hasFeedback == 1 ~ -0.45,
+  ) 
+  )%>% 
+  filter(honestidade <= 0.9, !is.na(profile)) %>%
+  group_by(honestidade, profile, profile_count) %>% 
+  summarise(uniq = (mean(uniqueValidators)),total = n()) 
+
+ecomony_3
+ggplot(data=ecomony_3, aes(y=uniq,x=honestidade*100,ymax = if_else(uniq==100,uniq,uniq*0.95), ymin = if_else(uniq*1.05+0.05<100,if_else(uniq==0,0,uniq*1.05+0.05),100), fill=profile))  +
+  geom_bar(stat="identity", position=position_dodge(),color="white")+
+  geom_text(aes(label=format(round(uniq, 1), nsmall = 1),hjust=profile_count    ), vjust=-0.2, color="black", size=3.5)+
+  geom_errorbar(position=position_dodge(8.6))  +
+  labs(x='Honesty rate (%)',  
+       y="Diversity of arbitrators (%)", 
        #title="Comparativo de Correlação entre Honestidade e Sucesso ", 
        #subtitle="(Sucesso da População, Taxa de Honestidade na População)",
-       fill="Com validação") +
+       fill="Models") +
   theme_minimal()+
   theme(plot.title = element_text(face="bold",size = "15"),
         plot.subtitle = element_text(size = "10"),
@@ -62,10 +103,38 @@ ggplot(data=ecomony_2, aes(y=acertou*100,x=honestidade*100,ymax = (acertou*97.5)
         axis.title.y = element_text(size="12"),
         axis.text.x = element_text(size="10"),
         axis.text.y = element_text(size="12"),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank(),
+        #panel.background = element_blank(),
         legend.position = "bottom",
         panel.border=element_blank())+
+  scale_x_continuous(breaks = seq(10, 90, by = 10),limits=c(0, 100)) +
   scale_fill_grey()
 
+
+ecomony_2
+ggplot(data=ecomony_2, aes(y=acertou*100,x=honestidade*100,ymax = if_else(acertou==1,acertou*100,acertou*97.5), ymin = if_else(acertou*102.5+2.5<100,if_else(acertou==0,0,acertou*102.5+2.5),100),  fill=profile))  +
+  geom_bar(stat="identity", position=position_dodge(),color="white")+
+  geom_text(aes(label=format(round(acertou*100, 1), nsmall = 1) ),  vjust=-0.1, color="black", size=3.5)+
+  geom_errorbar(position=position_dodge(8.6))  +
+  labs(x='Honesty Rate (%)',  
+       y="Population Success (%)", 
+       #title="Comparativo de Correlação entre Honestidade e Sucesso ", 
+       #subtitle="(Sucesso da População, Taxa de Honestidade na População)",
+       fill="Models") +
+  theme_minimal()+
+  theme(plot.title = element_text(face="bold",size = "15"),
+        plot.subtitle = element_text(size = "10"),
+        plot.caption = element_text(size="10"),
+        axis.title.y = element_text(size="12"),
+        axis.text.x = element_text(size="10"),
+        axis.text.y = element_text(size="12"),
+        panel.grid.minor = element_blank(),
+        panel.grid.major = element_blank(),
+        legend.position = "bottom",
+        panel.border=element_blank())+
+  scale_x_continuous(breaks = seq(10, 90, by = 10),limits=c(0, 100)) +
+  scale_fill_grey()
 
 #data = ecomony %>% 
 #  filter(honestidade <= 0.9)
@@ -87,3 +156,4 @@ ggplot(data=ecomony_2, aes(y=acertou*100,x=honestidade*100,ymax = (acertou*97.5)
 #  geom_hline(yintercept = mean(data)) + 
 #  geom_errorbar() + 
 #  facet_grid(. ~ type)
+
